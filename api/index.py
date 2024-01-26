dbaccc3bfedda6571a964c6e9ca179e29e17b4d3
GalihMrd <69835093+galihmrd@users.noreply.github.com>
2024-01-26 15:47:04 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,9 +1,14 @@
+import requests
+from flask import Flask, jsonify, make_response
 
 
+app = Flask(__name__)
+
+@app.route("/tiklydown", methods=["GET"])
 def tikly():
     try:
         BASE_URL = "https://api.tiklydown.eu.org"
-        r = requests.get(BASE_URL + f"/api/download?url={url}", timeout=8)
+        r = requests.get(BASE_URL + f"/api/download?url={request.args.get("url")}", timeout=10)
         return r.json()
     except BaseException:
         raise Exception("No results found!")
